83e1871922294ae84626ba60dd8db100cd91b532
altman <sunhui@datahunter.cn>
2019-01-03 16:30:29 +0800

    update

---
+++ tg/dhops.js
@@ -0,0 +1,30 @@
+const Telegraf = require('telegraf')
+const SocksAgent = require('socks5-https-client/lib/Agent');
+const socksAgent = new SocksAgent({
+  socksHost: process.env.SOCKS_HOST,
+  socksPort: process.env.SOCKS_PORT,
+//  socksUsername: config.proxy.login,
+//  socksPassword: config.proxy.psswd,
+});
+const { exec } = require('child_process');
+
+const users = {'743620537':'altman', '743620537a':'wxg', '743620537b':'xgc'}
+const bot = new Telegraf(process.env.DHOPS_BOT,{ telegram: { agent: socksAgent }  })
+//const bot = new Telegraf(process.env.DHOPS_BOT)
+bot.on('sticker', ctx => ctx.reply('👍'))
+bot.command('id', ctx => ctx.reply(`fromId=${ctx.from.id}\nfromUsername=${ctx.from.username}\nchatId=${ctx.chat.id}`))
+bot.command('ssh', ctx => {
+    fromId = ctx.from.id + ''
+    user = users[fromId]
+    console.log(new Date(), user)
+    exec(`usermod -s /bin/bash ${user}`, (err, stdout, stderr) => {
+      if (err) {
+        ctx.reply(`err:\n${err}`);
+      } else if (stderr) {
+        ctx.reply(`stderr:\n${stderr}`)
+      } else {
+        ctx.reply(`ssh登录已开放`)
+      }
+    });
+})
+bot.startPolling()
