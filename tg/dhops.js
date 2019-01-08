031a18dcfadf9f329f96e39848fdc7f4fd7c3d78
altman <sunhui@datahunter.cn>
2019-01-08 14:29:28 +0800

    update

--- tg/dhops.js
+++ tg/dhops.js
@@ -11,8 +11,26 @@ const { exec } = require('child_process');
 const users = {'743620537':'altman', '743620537a':'wxg', '788120538':'xgc'}
 const bot = new Telegraf(process.env.DHOPS_BOT,{ telegram: { agent: socksAgent }  })
 //const bot = new Telegraf(process.env.DHOPS_BOT)
+
+bot.use((ctx, next) => {
+    fromId = ctx.from.id + ''
+    user = users[fromId]
+    console.log(new Date(), user)
+    if (!user) {
+        ctx.reply(`对不起，我不认识你`);
+        return
+    }
+    const start = new Date()
+    return next(ctx).then(() => {
+    const ms = new Date() - start
+    console.log('Response time %sms', ms)
+    })
+})
+
 bot.on('sticker', ctx => ctx.reply('👍'))
+
 bot.command('id', ctx => ctx.reply(`fromId=${ctx.from.id}\nfromUsername=${ctx.from.username}`))
+
 bot.command('ssh', ctx => {
     fromId = ctx.from.id + ''
     user = users[fromId]
