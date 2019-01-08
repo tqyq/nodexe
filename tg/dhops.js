82be972d669a7c4bed4cd4db97d22baed54104b8
altman <sunhui@datahunter.cn>
2019-01-08 15:05:41 +0800

    update

--- tg/dhops.js
+++ tg/dhops.js
@@ -8,7 +8,7 @@ const socksAgent = new SocksAgent({
 });
 const { exec } = require('child_process');
 
-const users = {'743620537':'altman', '743620537a':'wxg', '788120538':'xgc'}
+const users = {'743620537a':'altman', '743620537a':'wxg', '788120538':'xgc'}
 const bot = new Telegraf(process.env.DHOPS_BOT,{ telegram: { agent: socksAgent }  })
 //const bot = new Telegraf(process.env.DHOPS_BOT)
 
@@ -31,10 +31,7 @@ bot.command('id', ctx => ctx.reply(`fromId=${ctx.from.id}\nfromUsername=${ctx.fr
 bot.command('ssh', ctx => {
     fromId = ctx.from.id + ''
     user = users[fromId]
-    console.log(new Date(), user)
-//    if (!ctx.state.auth) {
-//        return
-//    }
+    console.log('ssh enable', user)
     exec(`usermod -s /bin/bash ${user}`, (err, stdout, stderr) => {
       if (err) {
         ctx.reply(`err:\n${err}`);
