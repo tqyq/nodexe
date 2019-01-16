0cf5f6452b6766c9fe37e9a7e3f1c5e8d160c8ac
altman <sunhui@datahunter.cn>
2019-01-16 14:04:37 +0800

    update

--- tg/dhops.js
+++ tg/dhops.js
@@ -87,11 +87,12 @@ bot.action('top', async (ctx) => {
         if (err) {
             ctx.reply(err.stack)
         } else {
+            console.log("rows",rows)
             ctx.reply('选择主机', Extra.HTML().markup((m) =>
             m.inlineKeyboard(()=>{
                 keyboards = []
                 for (row in rows) {
-                    console.log(row)
+                    console.log('row',row)
                     keyboards.append(m.callbackButton('gp1', 'p_gp01'))
                 }
                 return keyboards
