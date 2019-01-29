7de6c190ce6e733496082ddc39cecc6131bac40f
altman <sunhui@datahunter.cn>
2019-01-29 14:38:39 +0800

    update

--- tg/dhops.js
+++ tg/dhops.js
@@ -40,8 +40,8 @@ bot.action('ssh', ctx => {
 })
 
 bot.on('text', (ctx) => {
-    let gp_conn = `gp连接${conn_count}`
-    let line1 = {'开启ssh':'ssh', '查负载':'top', gp_conn:'conn', 'gp状态':'gpstate'}
+    let line1 = {'开启ssh':'ssh', '查负载':'top', 'gp状态':'gpstate'}
+    line1[`gp连接${conn_count}`] = 'conn'
     const buttons = Object.keys(line1).map(key => Markup.callbackButton(key, line1[key]))
     ctx.reply('选择功能', Extra.HTML().markup((m) =>
         m.inlineKeyboard(buttons)))
