8c1dfe65a7c68e0484ece2c496a8eb2ed2026167
altman <sunhui@datahunter.cn>
2019-03-01 18:00:14 +0800

    update

--- wp/test.js
+++ wp/test.js
@@ -11,8 +11,11 @@ var wp = new WPAPI({
 })
 
 wp.posts().create({
-    title: 'Your Post Title',
-    content: 'Your post content',
+    title: '寒食',
+    content: `<a href="/tag/唐代">唐代</a>：<a href="/tag/韩翃">韩翃</a>
+
+春城无处不飞花，寒食东风御柳斜。
+日暮汉宫传蜡烛，轻烟散入五侯家。`,
 //    status: 'publish'
     status: 'draft'
 }).then(function( response ) {
