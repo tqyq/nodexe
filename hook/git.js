bfb58b32bee4c45674c27c073ae953f063350357
altman <sunhui@datahunter.cn>
2019-06-11 18:04:40 +0800

    update

--- hook/git.js
+++ hook/git.js
@@ -3,6 +3,7 @@ const app = express()
 const { exec } = require('child_process')
 
 app.get('/', function (req, res) {
+    console.log(JSON.stringify(req.headers))
     exec(`/root/git_dh_wiki.sh`, (err, stdout, stderr) => {
         res.send(err || stderr || stdout)
     })
@@ -10,9 +11,9 @@ app.get('/', function (req, res) {
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log("http://%s:%s", host, port)
+    console.log("http://%s:%s", host, port)
 
 })
